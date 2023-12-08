fc1a6b80091012ce8dfeba472cf3a71ed8601ac4
Stefan <ciobanustefan30@yahoo.com>
2023-12-08 16:39:30 +0200

    initial commit

---
+++ src/app/round/round.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-round',
+  templateUrl: './round.component.html',
+  styleUrl: './round.component.css'
+})
+export class RoundComponent {
+
+}
