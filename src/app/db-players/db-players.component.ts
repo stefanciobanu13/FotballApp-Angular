380e5b506df1356f8c64ce56960136a72c17c996
Stefan <ciobanustefan30@yahoo.com>
2024-02-03 18:47:10 +0200

    defined the deletion of a player in db

--- src/app/db-players/db-players.component.ts
+++ src/app/db-players/db-players.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
 interface Player {
+  id:number;
   firstName: String;
   lastName: String;
   grade: number;
@@ -16,17 +17,59 @@ interface Player {
 export class DbPlayersComponent implements OnInit {
   constructor(private http: HttpClient, private router: Router) {}
 
-  players: any[];
+  players: Player[];
 
   navigateToRoute() {
     this.router.navigate(['/AddPlayer']);
   }
 
   ngOnInit(): void {
-    this.http
-      .get<Player[]>('http://localhost:8080/players')
-      .subscribe((response) => {
+    this.fetchPlayers();
+  }
+
+  fetchPlayers() {
+    this.http.get<Player[]>('http://localhost:8080/players').subscribe(
+      (response) => {
         this.players = response;
-      });
+      },
+      (error) => {
+        console.error('Error fetching players:', error);
+      }
+    );
+  }
+
+  deletePlayer(id: number) {
+    const confirmDelete = window.confirm(
+      'Sunteti sigur(a) ca doriti sa stergeti acest jucator?'
+    );
+
+    if (confirmDelete) {
+      this.http.get<Player>(`http://localhost:8080/players/${id}`).subscribe(
+        (response) => {
+          if (response) {
+            this.http
+              .delete(`http://localhost:8080/players/${id}`, {
+                responseType: 'text',
+              })
+              .subscribe(
+                () => {
+                  console.log('Player deleted successfully');
+                  alert('Jucatorul a fost sters cu succes');
+                  this.fetchPlayers();
+                },
+                (error) => {
+                  console.error('Error in deleting the player:', error);
+                  alert('Eroare la stergerea jucatorului');
+                }
+              );
+          } else {
+            alert('Jucatorul nu a fost gasit.');
+          }
+        },
+        (error) => {
+          console.error('Error in checking player existence:', error);
+        }
+      );
+    }
   }
 }
